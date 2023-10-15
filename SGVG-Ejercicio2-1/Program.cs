a17cf95e01bc37c39ab13253246679af216dcc70
SGVG <SGVG@DESKTOP-57IOI3N>
2023-10-15 10:21:25 -0600

    Agregar archivos de proyecto.

---
+++ SGVG-Ejercicio2-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Escribe primer Número: ");
+        int num1 = int.Parse(Console.ReadLine());
+        if (num1 % 3 == 1)
+        {
+            Console.WriteLine("El residuo del Numero " + num1 + " entre 3 es igual a 1");
+        }
+        else
+        {
+            Console.WriteLine("El residuo no es igual a 1");
+        }
+    }
+}
